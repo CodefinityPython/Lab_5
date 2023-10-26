c418659acde9303bb24a6b1515821b5163214cb1
“Glen <valerii.hrebennikov@hebron-academy.com>
2023-10-26 16:52:23 +0300

    Solution

--- main.py
+++ main.py
@@ -5,13 +5,6 @@ class User:
         self.user_name = user_name
         self.password = password
 
-    def change_password(self, new_password):
-        self.password = new_password
-
-# Створюємо об'єкт user1
-user1 = User(1, "John Doe", "john_doe123", "password123")
-
-# Змінюємо пароль
-user1.change_password("new_password456")
-print(f"New Password: {user1.password}")
+def change_password(self, new_password):
+        self.password = new_password 
 
